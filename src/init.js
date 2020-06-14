5e0e1e8e9b4c156ae0c758ef86663013937f8f52
Kyle Bustard <bustardkyle@gmail.com>
2020-06-13 21:13:13 -0500

    Add UI interactions

--- src/init.js
+++ src/init.js
@@ -1,24 +1,23 @@
 import game from "./gameState";
-
-const TICK_RATE = 3000;
+import { TICK_RATE } from "./constants";
+import initButtons from "./buttons";
 
 async function init() {
   console.log("starting game");
+  initButtons(game.handleUserAction);
 
   let nextTimeToTick = Date.now();
 
   function nextAnimationFrame() {
     const now = Date.now();
-
     if (nextTimeToTick <= now) {
       game.tick();
       nextTimeToTick = now + TICK_RATE;
     }
-
     requestAnimationFrame(nextAnimationFrame);
   }
 
-  requestAnimationFrame(nextAnimationFrame);
+  nextAnimationFrame();
 }
 
 init();
