a9b8195c33db3bc01845ca9d6644b28b0113ee45
Kyle Bustard <bustardkyle@gmail.com>
2020-06-14 12:58:48 -0500

    Add new states

--- src/gameState.js
+++ src/gameState.js
@@ -1,14 +1,54 @@
 const gameState = {
   current: "INIT",
   clock: 1,
+  wakeTime: -1,
   tick() {
     this.clock++;
     console.log("clock", this.clock);
     return this.clock;
   },
+  startGame() {
+    console.log("hatching");
+    this.current = "HATCHING";
+    this.wakeTime = this.clock + 3;
+  },
+  wake() {
+    console.log("awoken");
+    this.current = "IDLING";
+    this.wakeTime = -1;
+  },
   handleUserAction(icon) {
-    console.log(icon);
+    if (
+      ["CELEBRATING", "FEEDING", "HATCHING", "SLEEPING"].includes(this.current)
+    ) {
+      return;
+    } else if (this.current === "INIT" || this.current === "DEAD") {
+      this.startGame();
+      return;
+    }
+
+    switch (icon) {
+      case "weather":
+        this.changeWeather();
+        break;
+      case "poop":
+        this.cleanupPoop();
+        break;
+      case "fish":
+        this.feed();
+        break;
+    }
+  },
+  changeWeather() {
+    console.log("changeWeather");
+  },
+  cleanupPoop() {
+    console.log("cleanupPoop");
+  },
+  feed() {
+    console.log("feed");
   },
 };
 
+export const handleUserAction = gameState.handleUserAction.bind(gameState);
 export default gameState;
